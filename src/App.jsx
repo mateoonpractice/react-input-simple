c17f71624bab9b92e7a5b422b0b193b1cc51d39b
Mateo Lopez <matheologu@gmail.com>
2025-09-03 19:22:38 -0600

    all images hosted in index

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import './App.css'
 
 function App() {
   const coverArtist = "https://raw.githubusercontent.com/mateoonpractice/react-input-simple/e6ca219c0690906b6634e1103197d6177bf9828a/src/images/triples-artist.jpg";
+  const logoKodigo = "https://raw.githubusercontent.com/mateoonpractice/react-input-simple/e6ca219c0690906b6634e1103197d6177bf9828a/src/kmusic.png";
+  const userAvatar = "https://raw.githubusercontent.com/mateoonpractice/react-input-simple/e6ca219c0690906b6634e1103197d6177bf9828a/src/user-bear.png";
+  const plnCover = "https://raw.githubusercontent.com/mateoonpractice/react-input-simple/e6ca219c0690906b6634e1103197d6177bf9828a/src/images/songs-covers/diablotriples.jpg";
 
   return (
     <>
     <nav className="nav-head">
       <div className="logo">
-        <img src="src/kmusic.png" alt="logo kodigo music"/>
+        <img src={logoKodigo} alt="logo kodigo music"/>
       </div>
         <div className="menu-head">
             <button className="menu-btn" onClick={() => window.location.href='musica.html'}>Inicio</button>
@@ -18,7 +21,7 @@ function App() {
         </div>
       {/* User Corner */}
       <div className="user-corner">
-        <img src="src/user-bear.png" alt="user"/>
+        <img src={userAvatar} alt="user"/>
         <p>User Name</p>
       </div>
     </nav>
@@ -92,7 +95,7 @@ function App() {
       <aside className="playing-module">
         <div className="playing-now">
           <h3>Reproduciendo</h3>
-          <img className="pln-cover" src="src/images/songs-covers/diablotriples.jpg" alt="cover"/>
+          <img className="pln-cover" src={plnCover} alt="cover"/>
           <p className="pln-title">Friend Zone</p>
           <p className="pln-artist">tripleS</p>
         </div>
